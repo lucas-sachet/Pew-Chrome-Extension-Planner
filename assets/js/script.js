b8dc03dee02257361511fe4567cbbddddfe9a337
Lucas Sachet <lucasachet@gmail.com>
2020-10-27 11:36:44 -0300

    Allow a user to add action item from input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,51 @@
+let addItemForm = document.querySelector('#addItemForm');
+let itemsList = document.querySelector('.actionItems');
+
+addItemForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let itemText = addItemForm.elements.namedItem('itemText').value;
+  if(itemText) {
+    renderActionItem(itemText);
+    addItemForm.elements.namedItem('itemText').value = '';
+  } 
+})
+
+const renderActionItem = (text) => {
+  let divElement = document.createElement('div');
+  let mainElement = document.createElement('div');
+  let checkElement = document.createElement('div');
+  let textElement = document.createElement('div');
+  let deleteElement = document.createElement('div');
+  divElement.classList.add('actionItem__item');
+  mainElement.classList.add('actionItem__main');
+  checkElement.classList.add('actionItem__check');
+  textElement.classList.add('actionItem__text');
+  deleteElement.classList.add('actionItem__delete');
+
+  checkElement.innerHTML =
+    `
+        <div class="actionItem__checkBox">
+          <i class="fas fa-check" aria-hidden="true"></i>
+        </div> 
+      `;
+
+  textElement.textContent = text;
+
+  deleteElement.innerHTML =
+    `
+      <i class="fas fa-times" aria-hidden="true"></i>
+    `;
+  mainElement.appendChild(checkElement);
+  mainElement.appendChild(textElement);
+  mainElement.appendChild(deleteElement);
+  divElement.appendChild(mainElement);
+  console.log(divElement)
+  console.log(itemsList);
+  itemsList.prepend(divElement);
+  console.log(itemsList);
+}
+
+
 var circle = new ProgressBar.Circle('#container', {
   color: '#7532a8',
   // This has to be the same size as the maximum width to
@@ -9,10 +57,16 @@ var circle = new ProgressBar.Circle('#container', {
   text: {
     autoStyleContainer: false
   },
-  from: { color: '#b24dff', width: 1 },
-  to: { color: '#7532a8', width: 4 },
+  from: {
+    color: '#b24dff',
+    width: 1
+  },
+  to: {
+    color: '#7532a8',
+    width: 4
+  },
   // Set default step function for all animate calls
-  step: function(state, circle) {
+  step: function (state, circle) {
     circle.path.setAttribute('stroke', state.color);
     circle.path.setAttribute('stroke-width', state.width);
 
@@ -28,4 +82,4 @@ var circle = new ProgressBar.Circle('#container', {
 circle.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 circle.text.style.fontSize = '2rem';
 
-circle.animate(1.0);  // Number from 0.0 to 1.0
+circle.animate(1.0); // Number from 0.0 to 1.0
