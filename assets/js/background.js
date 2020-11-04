fe49b074b78e3631dc82356a39d7a377ed95601f
Lucas Sachet <lucasachet@gmail.com>
2020-11-04 15:39:52 -0300

    Initialize action items on instal of Extension

--- assets/js/background.js
+++ assets/js/background.js
@@ -6,6 +6,14 @@ chrome.contextMenus.create({
   "contexts": ["all"],
 })
 
+chrome.runtime.onInstalled.addListener(( details ) => {
+  if(details.reason == 'install') {
+    chrome.storage.sync.set({
+      actionItems : []
+    })
+  }
+})
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if(info.menuItemId == "linkSiteMenu") {
     actionItemUtils.addQuickActionItem('quick-action-4', 'Read this site', tab, () => {
